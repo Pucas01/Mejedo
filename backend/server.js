60f63c7eb501fac08bab7d85a4973f93f17bfe23
Pucas01 <129163556+Pucas01@users.noreply.github.com>
2025-11-03 21:12:36 +0100

    Im so sad

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import dotenv from "dotenv";
 import spotifyRoute from "./routes/spotify.js"
 import guestbook from "./routes/guestbook.js"
 import counter from "./routes/moeCounter.js"
@@ -35,7 +34,6 @@ app.use(session({
 }));
 
 
-app.use(express.json());
 app.use("/api/spotify-now-playing", spotifyRoute);
 app.use("/api/guestbook", guestbook);
 app.use("/api/counter", counter);
@@ -43,7 +41,6 @@ app.use("/api/users", users);
 app.use("/api/projects", projectsRouter);
 app.use("/api/shitposts", shitpostsRouter);
 app.use("/api/upload", uploadRouter);
-app.use("/uploads", express.static("public/uploads"));
 
 // Start server
 app.listen(PORT, () => {
