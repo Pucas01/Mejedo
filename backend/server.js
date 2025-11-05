e55bd27697992d5c7cb188e24580a9e181a16018
Pucas01 <129163556+Pucas01@users.noreply.github.com>
2025-11-05 20:50:48 +0100

    OK THIS TIME RIGHT

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import session from "express-session"
 import cors from "cors";
 import * as crypto from 'crypto';
 import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
